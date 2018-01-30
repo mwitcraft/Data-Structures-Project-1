4baed7a3388486fb18ef11b8291e3a6a3c611465
Mason Witcraft <mwitcraft11@gmail.com>
2018-01-30 12:04:07 -0600

    PROGRESS: Can now set row name - still a little messy, but works. Added the Visual C++ project file. Renamed Test.cpp to Constructor and display. Display will need to be updated.

--- ConstructorAndDisplay.cpp
+++ ConstructorAndDisplay.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -13,13 +14,17 @@ public:
 		numRows = rows;
 		numCols = cols;
 
+		//Initializes table as an int*[] with the size of numRows
 		table = new int*[numRows];
 		for (int i = 0; i < numRows; i++) {
+			//Inside of each int*, a new int*[] of size numCols is created
 			table[i] = new int[numCols];
 		}
 
+		//Steps through the number of rows and columns to make a DataFrame
 		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numCols; j++) {
+				//Each possible entry in the DataFrame is initially given the value of 0
 				table[i][j] = 0;
 			}
 		}
@@ -30,10 +35,13 @@ public:
 		cout << "Number of columns " << numCols << endl;
 		cout << "***************************" << endl;
 
+		//Steps through each column in each row and prints out the contents of the pointer
 		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numCols; j++) {
+				//Space at end of value for formatting
 				cout << table[i][j] << " ";
 			}
+			//Ends line at end of each row for formatting
 			cout << endl;
 		}
 	}
@@ -45,3 +53,4 @@ int main() {
 
 	return 0;
 }
+*/
--- Test1.cpp
+++ Test1.cpp
@@ -1,130 +1,77 @@
-/*
 #include <iostream>
 using namespace std;
 
-class DataFrame {
-protected:
-	int**   table;
-	int     numRows;
-	int     numCols;
-	char**  colNames;
-	char**  rowNames;
+class FrameData {
 
 public:
-	//Constructors
-	DataFrame() {};
-	DataFrame(int rows, int cols) {
+
+	int** table;
+	int numRows;
+	int numCols;
+	char** colNames;
+	char** rowNames;
+
+	FrameData(int rows, int cols) {
 		numRows = rows;
 		numCols = cols;
-	};
 
-	//Output method
-	void display();
+		table = new int*[numRows];
+		for (int i = 0; i < numRows; i++) {
+			table[i] = new int[numCols];
+		}
 
-	//Setters
-	void setRowName(int row, char* name);
-	void setColName(int col, char* name);
-	int* operator[](int i); //Get row i
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 0; j < numCols; j++) {
+				table[i][j] = 0;
+			}
+		}
 
-	//Getters
-	char** getRowNames();
-	char** getColNames();
-	int getNumberRows();
-	int getNumberCols();
-	DataFrame* getColumns(int* columns, int cLen);
-	DataFrame* getRows(int* rows, int rLen);
-	DataFrame* getRowsCols(int* rows, int rLen, int* cols, int cLen);
+		rowNames = new char*[numRows];
+		for (int i = 0; i < numRows; i++) {
+			rowNames[i] = (char*)"*";
+		}
+	}
 
-	//Destructor
-	~DataFrame();
+	void setRowName(int row, char* name) {
+		cout << "Setting row " << row << " to " << name << endl;
+		for (int i = 0; i <= numRows; i++) {
+			if (i == row) {
+				rowNames[i-1] = name;
+			}
+		}
+	}
+
+	void setColName(int col, char* name) {
+
+	}
+
+	void display() {
+		cout << "Number of rows " << numRows << endl;
+		cout << "Number of columns " << numCols << endl;
+		cout << "***************************" << endl;
+
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 0; j < numCols; j++) {
+				cout << table[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	void displayName() {
+		for (int i = 0; i < numRows; i++) {
+			cout << rowNames[i] << endl;
+		}
+	}
 };
 
 int main() {
-	int nCols;
-	int nRows;
-	int selectC[3];
-	int selectR[10];
+	FrameData* fd = new FrameData(8, 3);
 
-	//Read dataframe from input
-	//First line: 2 numbers seperated by space;
-	//			  first number is number of rows (r) and
-	//			  second number is number of columns (c)
+	(*fd).setRowName(2, (char*)"Mason");
+	fd->setRowName(1, (char*)"Reece");
+	fd->setRowName(8, (char*)"Jessie");
+	fd->displayName();
 
-	cin >> nRows >> nCols;
-	DataFrame* firstDF = new DataFrame(nRows, nCols);
-
-	//Second line: strings seperated by a comma (nCols of them); representing column names
-	//Third line: string seperated by a comma (nRows of them); representing row names
-	//Fourth line and more: nCols number of integers in each of the nRows rows seperated by a space
-	//						in between integers in the same row
-
-	//TODO: complete code for the above comment block^^^
-
-	//using the display method, print (in the same format as the input):
-	// - column names of the dataframe
-	// - row names of the dataframe
-	// - contents of the table in the dataframe
-
-	//TODO: complete code for the above comment block^^^
-
-	//Execute the following code when i think its complete??
-
-	/*
-	for (int i = 0; i < 3; i++) {
-	cin >> selectC[i];
-	}
-
-	DataFrame* tempColumns = (*firstDF).getColumns(selectC, 3);
-
-	(*tempColumns).display();
-
-	//Change the row names of select rows
-
-	(*tempColumns).setRowName(2, "Jack");
-	(*tempColumns).setRowName(3, "Peter");
-
-	(*tempColumns).display();
-
-	//Read the row numbers that you want to extract
-
-	for (int i = 0; i < 10; i++) {
-	cin >> selectR[i];
-	}
-
-	DataFrame* tempRows = (*firstDF).getRows(selectR, 10);
-
-	(*tempRows).display();
-
-	//Change the column names of selected columns
-
-	(*tempRows).setColName(2, "Scores");
-	(*tempRows).setColName(3, "Values");
-
-	(*tempRows).display();
-
-	//Extract the rows in selectR and columns in selectC
-
-	DataFrame* tempColsRows = (*firstDF).getRowsCols(selectR, 10, selectC, 3);
-
-	(*tempColsRows).display();
-
-	delete tempRows;
-	delete tempCols;
-	delete tempColsRows;
-
-	//Sample code - must execute
-
-	DataFrame* myTable = new DataFrame(5, 5);
-
-	for (int i = 0; i < 5; i++) {
-	for (int j = 0; j < 5; j++) {
-	(*myTable)[i][j] = i * j;
-	}
-	}
-
-	(*myTable).display();
-
-	delete myTable;
-
-	*/
-
+	return 0;
+}
